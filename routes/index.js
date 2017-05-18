f2890da21cf03f71a76591c4208345b18b9e6eb7
Julian Kraus <julian.kraus@gmail.com>
2017-05-18 22:52:18 +0200

    added debug mode

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res) {
-    var trier = {
-        lat: 49.749992,
-        long: 6.6371433,
-        zoom: 13
-    };
-
-    // now we use the templating capabilities of express and call our template to render the view, and pass a few parameters to it
-    res.render("home", { title: "Hausarbeit", lat: trier.lat, lon: trier.long, zoom: trier.zoom });
+    res.render("home");
 });
 
 module.exports = router;
