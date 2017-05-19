ac843de4f00101dc00bbf0f6548a1d2c56fdfd1f
Julian Kraus <julian.kraus@gmail.com>
2017-05-19 05:22:18 +0200

    added list

--- public/javascripts/Main.js
+++ public/javascripts/Main.js
@@ -1,13 +1,13 @@
 /**
  * Created by Julian on 18.05.17.
  */
-var pr = document.querySelector( '.paginate.left' );
-var pl = document.querySelector( '.paginate.right' );
+let pr = document.querySelector( '.paginate.left' );
+let pl = document.querySelector( '.paginate.right' );
 
 pr.onclick = slide.bind( this, -1 );
 pl.onclick = slide.bind( this, 1 );
 
-var index = 0, total = 5;
+let index = 0, total = 5;
 
 function slide(offset) {
     index = Math.min( Math.max( index + offset, 0 ), total - 1 );
