456b22a8a4eef676a62466538b7d9438d4e7e5d0
Julian Kraus <julian.kraus@gmail.com>
2017-06-05 11:36:42 +0200

    added elevation chart

--- public/javascripts/Foo.js
+++ public/javascripts/Foo.js
@@ -1,1 +1,28 @@
 // Hier könnte ihre Werbung stehen
+//let fs = require("fs");
+
+/*var obj = fs.readdirSync("../json", "utf8");
+var jsonFiles = [];
+var elevations = [];
+
+// copy json files from dir
+obj.forEach(function (file) {
+    jsonFiles.push(JSON.parse(fs.readFileSync("../json/" + file, "utf8")));
+});
+
+// store elevation coordinates
+jsonFiles.forEach(function (data) {
+    data.features[0].geometry.coordinates.forEach(function (coordinates) {
+        console.log(coordinates[2]);
+    })
+})
+
+jsonFiles[0].features[0].geometry.coordinates.forEach(function (coordinates) {
+    elevations[counter] = [counter, coordinates[2]];
+    counter++;
+});
+
+console.log(elevations);
+
+//console.log(jsonFiles[0].features[0].geometry.coordinates[0]);
+    */
